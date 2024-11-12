851493aa55255035859a08d6db350e5b38604ed7
Jim Blandy <jimb@red-bean.com>
2024-11-12 15:24:07 -0800

    Deserialize display title

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
-use std::collections::HashMap;
+#[allow(dead_code)]
+#[derive(Debug, serde::Deserialize)]
+struct ParseResponse {
+    parse: DisplayTitle,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, serde::Deserialize)]
+struct DisplayTitle {
+    title: String,
+    pageid: u64,
+    displaytitle: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://httpbin.org/ip")
+    let url = "https://en.wikipedia.org/w/api.php\
+               ?action=parse\
+               &format=json\
+               &page=Rust_(programming_language)\
+               &prop=displaytitle\
+               &formatversion=2";
+    let resp = reqwest::get(url)
         .await?
-        .json::<HashMap<String, String>>()
+        .json::<ParseResponse>()
         .await?;
     println!("{resp:#?}");
     Ok(())
