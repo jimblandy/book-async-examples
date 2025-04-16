e9083e11cf88fa1cfbbf318f02bc2d7de54a4038
Jim Blandy <jimb@red-bean.com>
2025-04-15 20:40:33 -0700

    simplified

--- wikipedia-reachable/src/bin/unthrottled.rs
+++ wikipedia-reachable/src/bin/unthrottled.rs
@@ -20,10 +20,6 @@ impl Traversal {
             errors: Mutex::new(Vec::new()),
         }
     }
-
-    async fn save_error(&self, error: anyhow::Error) {
-        self.errors.lock().unwrap().push(error);
-    }
 }
 
 use std::sync::Arc;
@@ -33,7 +29,7 @@ impl Traversal {
         self: Arc<Self>,
         page: String,
         depth: usize,
-        join_set: &mut tokio::task::JoinSet<Result<()>>,
+        join_set: &mut tokio::task::JoinSet<()>,
     ) {
         join_set.spawn(self.visit(page, depth));
     }
@@ -41,33 +37,28 @@ impl Traversal {
 
 impl Traversal {
     /// Visit all pages reachable from `page` within `depth` links.
-    async fn visit(self: Arc<Self>, page: String, depth: usize) -> Result<()> {
+    async fn visit(self: Arc<Self>, page: String, depth: usize) {
         if !self.seen.lock().unwrap().insert(page.clone()) {
-            return Ok(());
+            return;
         }
 
         if depth == 0 {
-            return Ok(());
+            return;
         }
+
+        let links = match links::page_links(&page).await {
+            Ok(links) => links,
+            Err(error) => {
+                self.errors.lock().unwrap().push(error);
+                return;
+            }
+        };
 
         let mut join_set = tokio::task::JoinSet::new();
-        for outgoing_link in links::page_links(&page).await? {
-            let new_self = Arc::clone(&self);
-            new_self.spawn_visit(outgoing_link, depth - 1, &mut join_set);
+        for link in links {
+            Arc::clone(&self).spawn_visit(link, depth - 1, &mut join_set);
         }
-
-        // All subtasks are in progress at this point, but almost all
-        // are probably blocked waiting for a network response.
-
-        while let Some(subtask_result) = join_set.join_next().await {
-            match subtask_result {
-                Err(join_error) => self.save_error(join_error.into()).await,
-                Ok(Err(visit_error)) => self.save_error(visit_error).await,
-                Ok(Ok(())) => {}
-            }
-        }
-
-        Ok(())
+        join_set.join_all().await;
     }
 }
 
@@ -76,7 +67,7 @@ async fn main() -> Result<()> {
     let traversal = Arc::new(Traversal::new());
 
     let start = "Rust (programming language)".to_string();
-    Arc::clone(&traversal).visit(start.clone(), 2).await?;
+    Arc::clone(&traversal).visit(start, 2).await;
 
     let traversal = Arc::into_inner(traversal).unwrap();
     let seen = traversal.seen.into_inner().unwrap();
