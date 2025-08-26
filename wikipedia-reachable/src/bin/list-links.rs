62164487b2389540d9ea1a0dc2ad7e89598e446c
Jim Blandy <jimb@red-bean.com>
2025-08-25 18:35:00 -0700

    list-links: tweak parameter name

--- wikipedia-reachable/src/bin/list-links.rs
+++ wikipedia-reachable/src/bin/list-links.rs
@@ -2,11 +2,11 @@ use wikipedia_reachable::links::page_links;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let Some(url) = std::env::args().nth(1) else {
-        anyhow::bail!("Usage: list-links URL");
+    let Some(title) = std::env::args().nth(1) else {
+        anyhow::bail!("Usage: list-links TITLE");
     };
 
-    for link in page_links(&url).await? {
+    for link in page_links(&title).await? {
         println!("{link}");
     }
 
